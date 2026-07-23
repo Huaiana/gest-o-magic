0cc6d5b24fb796341987386f7d6eb14ced1e33c0
huaydarc@gmail.com <huaydarc@gmail.com>
2026-07-23 17:50:55 -0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed:

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,6 @@ app.get('/', (req: Request, res: Response) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000')
+app.listen(8080, () => {
+  console.log('Servidor rodando em http://localhost:8080')
 })
--- src/server.ts
+++ src/server.ts
@@ -35,6 +35,6 @@ app.post("/movimentos", movimentoEstoqueController.criar);
 app.get("/relatorios", relatorioController.gerar);
 
 // Inicialização do servidor
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta http://localhost:3000");
+app.listen(8080, () => {
+    console.log("Servidor rodando na porta http://localhost:8080");
 });
